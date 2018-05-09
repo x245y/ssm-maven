20bab7102d5bd69727f60687a4f2fd3a1b8ecc17
xy <xiongying@joyschool.cn>
2018-05-09 20:32:04 +0800

    本工程为基于maven的ssm框架模板工程，首次提交，目前已经包含： 1、ssm框架 2、json数据接收和返回

---
+++ src/main/java/com/how2java/controller/CategoryController.java
@@ -0,0 +1,54 @@
+package com.how2java.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.support.PagedListHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.how2java.domain.api.Person;
+import com.how2java.pojo.Category;
+import com.how2java.service.CategoryService;
+import com.how2java.util.Page;
+
+// 告诉spring mvc这是一个控制器类
+@Controller
+@RequestMapping("")
+public class CategoryController {
+	@Autowired
+	CategoryService categoryService;
+
+	@RequestMapping("listCategory")
+	public ModelAndView listCategory(Page page){
+		ModelAndView mav = new ModelAndView();
+		PageHelper.offsetPage(page.getStart(),5);
+		List<Category> cs= categoryService.list();
+		int total = (int) new PageInfo<>(cs).getTotal();
+		
+		page.caculateLast(total);
+		// 放入转发参数
+		mav.addObject("cs", cs);
+		// 放入jsp路径
+		mav.setViewName("listCategory");
+		return mav;
+	}
+	@RequestMapping(value="post")
+	public @ResponseBody List<Category> listCategory1(@RequestBody Person p){
+		List<Category> cs= categoryService.list();
+	    return cs;  
+	}
+	@RequestMapping(value="get", method = RequestMethod.GET)
+	public @ResponseBody List<Category> listCategory2(@RequestParam String id, String name){
+		List<Category> cs= categoryService.list();
+	    return cs;  
+	}
+}
